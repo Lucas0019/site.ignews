f3d6fec97f0529c3c98d45f0d48837f2d0557ca3
lucas <lxavier.00@outlook.com>
2023-04-30 23:59:42 -0300

    feat(test): implemented test in the componete of acticve link and adjusted JEST conigurations

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,12 @@
-module.exports = {
-    testIgnorePatterns: ['/node_modules/', '/.next/'],
+const config = {
+    verbose: true,
+    testPathIgnorePatterns: ['/node_modules/', '/.next/'],
     setupFilesAfterEnv: ['<rootDir>/src/tests/setupTests.ts'],
     transform: {
         '^.+\\.(js|jsx|ts|tsx)$': '<rootDir>/node_modules/babel-jest',
     },
     testEnvironment: 'jsdom',
 }
+
+
+module.exports = config
